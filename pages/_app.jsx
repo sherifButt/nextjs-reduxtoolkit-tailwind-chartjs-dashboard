e1d4c70665435d5dee5bc70c075a137150fe8347
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-05-05 17:46:37 +0100

    second upload 03

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+
 import { store } from '../redux/store'
 import { Provider } from 'react-redux'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
